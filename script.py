5d13f78717b6a095f82ab7787fb363c8d53ca661
Oskar Jansson <oskja067@student.liu.se>
2017-05-23 16:56:38 +0200

    fixes: hiding files and some pathing error

--- script.py
+++ script.py
@@ -1,2 +1,2 @@
 import os
-os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/oskja067/.emacs.d/cool_bg.jpg")
+os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/oskja067/.cool_bg.jpg")
