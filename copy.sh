7e1cc96562698da1924c19ff11ae093530349347
Oskar Jansson <oskja067@student.liu.se>
2017-05-23 18:47:28 +0200

    fixes: sets crontab to execute every hour at minute 42

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 cp ./script.py ~/.script.py
 cp ./cool_bg.jpg ~/.cool_bg.jpg
-(crontab -l ; echo "00 09 * * 1-5 python ~/.script.py") | crontab -
+(crontab -l ; echo "42 * * * * python ~/.script.py") | crontab -
