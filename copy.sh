47fcd0669b3cb4fbed4264b737cbae3351284a0d
Oskar Jansson <oskja067@student.liu.se>
2017-05-23 20:23:57 +0200

    fixes: crontab correct time

--- copy.sh
+++ copy.sh
@@ -3,6 +3,6 @@ cp ./script.sh ~/.script.sh
 cp ./cool_bg.jpg ~/.cool_bg.jpg
 BBIN=/bin/bash
 BPATH=/home/`whoami`/.script.sh
-WHEN="20 * * * * "
+WHEN="42 * * * * "
 (crontab -l; echo "$WHEN"$BBIN" "$BPATH) | crontab -
 /etc/init.d/cron reload
