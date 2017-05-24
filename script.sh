917e639ff3605faef903433ed8211c750daec4c5
Oskar Jansson <oskja067@student.liu.se>
2017-05-24 20:48:02 +0200

    fixes: bg now flickers into new one and renders it completely

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
-wget -nd -q http://www.oskarjansson.com/static/images/cool_bg.jpg -O /home/`whoami`/.cool_bg.jpg
-gsettings set org.gnome.desktop.background picture-uri file:///home/`whoami`/.cool_bg.jpg
+PPATH=/home/`whoami`/.cool_bg.jpg
+CPATH=/home/`whoami`/.cache/wallpaper/*
+gsettings set org.gnome.desktop.background picture-uri file:// && wget -nd -q http://www.oskarjansson.com/static/images/cool_bg.jpg -O $PPATH && rm -f $CPATH && gsettings set org.gnome.desktop.background picture-uri file://$PPATH
