ac1d30226ddadc65d9bcfb7a7da1d535790d8991
Oskar Jansson <oskja067@student.liu.se>
2017-05-24 16:27:46 +0200

    adds: script now wgets the bg

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+wget -nd -q http://i.imgur.com/CtOHvRE.jpg -O home/`whoami`/.cool_bg.jpg
 gsettings set org.gnome.desktop.background picture-uri file:///home/`whoami`/.cool_bg.jpg
