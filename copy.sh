bf362db06ef4d3669ac78561d7da64066428a456
Oskar Jansson <oskja067@student.liu.se>
2017-05-24 17:50:28 +0200

    removes: remove.sh and copy.sh since they're moved into wpu.sh

--- copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp ./script.sh ~/.script.sh
-BBIN=/bin/bash
-BPATH=/home/`whoami`/.script.sh
-WHEN="42 * * * * "
-(crontab -l 2> /dev/null; echo "$WHEN"$BBIN" "$BPATH) | crontab -
-/etc/init.d/cron reload > /dev/null
--- remove.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-BPATH=/home/`whoami`/.script.sh
-IPATH=/home/`whoami`/.cool_bg.jpg
-rm -f $BPATH $IPATH
-
-crontab -l | grep -v $BPATH | crontab -
