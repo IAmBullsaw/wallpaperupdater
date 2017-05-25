01ed9008ef0235992a4de7d4ab6e590fcb5891f2
Oskar Jansson <oskja067@student.liu.se>
2017-05-25 21:01:12 +0200

    fixes: will only update IF internet exists

--- script.sh
+++ script.sh
@@ -3,4 +3,10 @@ PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 PPATH=/home/`whoami`/.cool_bg.jpg
 CPATH=/home/`whoami`/.cache/wallpaper/*
-gsettings set org.gnome.desktop.background picture-uri file:// && wget -nd -q http://www.oskarjansson.com/static/images/cool_bg.jpg -O $PPATH && rm -f $CPATH && gsettings set org.gnome.desktop.background picture-uri file://$PPATH
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [ $? -eq 0 ]; then
+    gsettings set org.gnome.desktop.background picture-uri file:// && \
+        wget -nd -q http://www.oskarjansson.com/static/images/cool_bg.jpg -O $PPATH && \
+        rm -f $CPATH && \
+        gsettings set org.gnome.desktop.background picture-uri file://$PPATH
+fi
